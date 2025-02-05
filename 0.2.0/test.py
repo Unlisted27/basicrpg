d7aed1e8c4e4b4563a9543e35f7fe9e1ce995790
Unlisted27 <archerhpo@gmail.com>
2025-02-05 14:16:02 -0400

    0.2.0 release

--- 0.2.0/test.py
+++
@@ -1,6 +0,0 @@
-import basicrpg
-Human = basicrpg.race("Human",0,0,0,0)
-Commoner = basicrpg.profession("Commoner")
-player = basicrpg.character(Human,Commoner,basicrpg.genname())
-
-print("You are on a dusty trail")
