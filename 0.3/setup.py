c497d8914ef74bd27e41d44c5446a367510c8a32
Unlisted27 <archerhpo@gmail.com>
2025-03-28 16:11:55 -0300

    0.3.2

--- 0.3/setup.py
+++ 0.3/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="basicrpg",
-    version="0.3.1",
+    version="0.3.2",
     description="Python library for making RPG games, clean and easy.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
